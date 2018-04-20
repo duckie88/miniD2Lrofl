b9d65591d3901ab44847500e92775de4fbfd7dd7
Tam Tran <trantam231995@yahoo.com>
2018-04-20 13:46:57 -0700

    Copied files from prev projects over

--- main.php
+++ main.php
@@ -1,1 +1,40 @@
-<?php
+<!DOCTYPE html>
+<!-- 
+Stephanie Marcellin, Tam Tran
+Final Project - Mini D2L Class website
+-->
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Course Website</title>
+	<link rel="stylesheet" type="text/css" href="classSite.css" />
+</head>
+<body>
+	<?php
+		// Include class and start session
+		session_start ();
+	?>
+
+	<h1 class="title">ECE220</h1>
+	<a href="register.php">Register</a> 
+	<a href="login.php">Login</a> 
+	<?php 
+		if (isset($_SESSION['user'])) {
+			echo "<form name=\"logout\" method=\"post\"><input name=\"logout\" type=\"submit\" value=\"Log Out\"></form>";
+		}
+	?>
+	<div id="toChange"></div>
+	
+	<script>
+		// TODO Functions if needed
+		
+	</script>
+	
+	<?php 
+		if (isset($_POST['logout'])) {
+			unset($_SESSION['user']);
+			header("Location: main.php");
+		}
+	?>
+</body>
+</html>
