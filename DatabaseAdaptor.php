128da6b47b53626d784d32eb659fc8fc87439580
Tam Tran <trantam231995@yahoo.com>
2018-04-25 12:16:50 -0700

    copy pasta'd

--- DatabaseAdaptor.php
+++ DatabaseAdaptor.php
@@ -22,17 +22,22 @@
 	  	
 	  	public function checkUser($username){
 	  	    $stmt = $this->DB->prepare("SELECT * FROM users WHERE username='" . $username . "';");
+	  	    $stmt->bindParam('username', $username);
 	  	    $stmt->execute();
 	  	    return $stmt->fetchAll( PDO::FETCH_ASSOC );
 	  	}
 	  	
 	  	public function addUser($username, $hash, $first_name){
 	  	    $stmt = $this->DB->prepare("INSERT INTO users (username, hash, first_name) values('" . $username . "', '" . $hash . "', '" . $first_name . "');");
+	  	    $stmt->bindParam('username', $username);
+	  	    $stmt->bindParam('hash', $hash);
+	  	    $stmt->bindParam('first_name', $first_name);
 	  	    $stmt->execute();
 	  	}
 	  	
 	  	public function getGrades($user) {
 	  	    $stmt = $this->DB->prepare( "SELECT * FROM (SELECT * FROM users JOIN grades ON user_id = grades_id) AS temp WHERE username = '" . $user . "';");
+	  	    $stmt->bindParam('user', $user);
 	  	    $stmt->execute();
 	  	    return $stmt->fetchAll( PDO::FETCH_ASSOC );
 	  	}
